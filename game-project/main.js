840edad2aa51244a7fa2949fb2b7058e7ff6349e
Thomas Helderop <thomashelderop@Thomass-MacBook-Air.local>
2020-06-11 12:21:58 -0600

    functional wall bounces

--- game-project/main.js
+++ game-project/main.js
@@ -17,11 +17,13 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById("starting-screen").style.visibility = "hidden"
         }
 
-    // Squares
+    // Square constructor
     function CrawlerSquare(color, width, height) {
         this.x = Math.floor(Math.random() * (canvas.width -25))
         this.y = Math.floor(Math.random() * (canvas.height - 25))
         this.color = color
+        this.dx = 1
+        this.dy = 1
         this.width = width
         this.height = height
         this.render = function() {
@@ -40,15 +42,13 @@ document.addEventListener('DOMContentLoaded', function() {
     greenSquare.render()
     yellowSquare.render()
 
-    // Circles
+    // Circle constructor
     function CrawlerCircle(color) {
-        // ctx.beginPath()
-        // ctx.arc(Math.floor(Math.random() * (canvas.width - 25)), Math.floor(Math.random() * (canvas.height - 25)),12.5,0,Math.PI*2,true)
-        // ctx.closePath()
-        // ctx.fill()
         this.x = Math.floor(Math.random() * (canvas.width - 25))
         this.y = Math.floor(Math.random() * (canvas.height - 25))
         this.color = color
+        this.dx = 1
+        this.dy = 1
         this.render = function() {
                 ctx.fillStyle = this.color
                 ctx.beginPath()
@@ -68,57 +68,369 @@ document.addEventListener('DOMContentLoaded', function() {
     greenCircle.render()
     yellowCircle.render()
 
-    function moveObject() {
+    function moveRedSquare() {
+
         ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-        redSquare.x += 1
-        redSquare.y += 1
-        if (redSquare.x > canvas.width) {
-            redSquare.x += -1
-        } else if (redSquare.x < 0) {
-            redSquare.x += 1
+        if (redSquare.dx === 1) {
+            if (redSquare.x + redSquare.width >= canvas.width) {
+                redSquare.dx += -1
+            }
+        } else if (redSquare.dx === -1) {
+            if (redSquare.x - redSquare.width <= 0) {
+                redSquare.dx += 1
+            }
+        }
+        if (redSquare.x + redSquare.width >= canvas.width) {
+            redSquare.dx = -1
         }
 
-        if (redSquare.y > canvas.height) {
-            redSquare.y += -1
-        } else if (redSquare.y < 0) {
-            redSquare.y += 1
+        if (redSquare.x - redSquare.width <= 0) {
+            redSquare.dx = 1
         }
 
-        blueSquare.x += 1
-        blueSquare.y += 1
+        if (redSquare.dy === 1) {
+            if (redSquare.y + redSquare.height >= canvas.height) {
+                redSquare.dy += -1
+            }
+        } else if (redSquare.dy === -1) {
+            if (redSquare.y - redSquare.height <= 0) {
+                redSquare.dy += 1
+            }
+        }
 
-        greenSquare.x += 1
-        greenSquare.y += 1
+        if (redSquare.y + redSquare.height >= canvas.height) {
+            redSquare.dy = -1
+        }
 
-        yellowSquare.x += 1
-        yellowSquare.y += 1
+        if (redSquare.y - redSquare.height <= 0) {
+            redSquare.dy = 1
+        }
 
-        redCircle.x += 1
-        redCircle.y += 1
-
-        blueCircle.x += 1
-        blueCircle.y += 1
-
-        greenCircle.x += 1
-        greenCircle.y += 1
-
-        yellowCircle.x += 1
-        yellowCircle.y += 1
-
-        redCircle.render()
-        blueCircle.render()
-        greenCircle.render()
-        yellowCircle.render()
+        redSquare.x += redSquare.dx
+        redSquare.y += redSquare.dy
 
         redSquare.render()
+
+    }
+
+    function moveBlueSquare() {
+
+        if (blueSquare.dx === 1) {
+            if (blueSquare.x + blueSquare.width >= canvas.width) {
+                blueSquare.dx += -1
+            }
+        } else if (blueSquare.dx === -1) {
+            if (blueSquare.x - blueSquare.width <= 0) {
+                blueSquare.dx += 1
+            }
+        }
+        if (blueSquare.x + blueSquare.width >= canvas.width) {
+            blueSquare.dx = -1
+        }
+
+        if (blueSquare.x - blueSquare.width <= 0) {
+            blueSquare.dx = 1
+        }
+
+        if (blueSquare.dy === 1) {
+            if (blueSquare.y + blueSquare.height >= canvas.height) {
+                blueSquare.dy += -1
+            }
+        } else if (blueSquare.dy === -1) {
+            if (blueSquare.y - blueSquare.height <= 0) {
+                blueSquare.dy += 1
+            }
+        }
+
+        if (blueSquare.y + blueSquare.height >= canvas.height) {
+            blueSquare.dy = -1
+        }
+
+        if (blueSquare.y - blueSquare.height <= 0) {
+            blueSquare.dy = 1
+        }
+
+        blueSquare.x += blueSquare.dx
+        blueSquare.y += blueSquare.dy
+
         blueSquare.render()
+
+    }
+
+    function moveGreenSquare() {
+
+        if (greenSquare.dx === 1) {
+            if (greenSquare.x + greenSquare.width >= canvas.width) {
+                greenSquare.dx += -1
+            }
+        } else if (greenSquare.dx === -1) {
+            if (greenSquare.x - greenSquare.width <= 0) {
+                greenSquare.dx += 1
+            }
+        }
+        if (greenSquare.x + greenSquare.width >= canvas.width) {
+            greenSquare.dx = -1
+        }
+
+        if (greenSquare.x - greenSquare.width <= 0) {
+            greenSquare.dx = 1
+        }
+
+        if (greenSquare.dy === 1) {
+            if (greenSquare.y + greenSquare.height >= canvas.height) {
+                greenSquare.dy += -1
+            }
+        } else if (greenSquare.dy === -1) {
+            if (greenSquare.y - greenSquare.height <= 0) {
+                greenSquare.dy += 1
+            }
+        }
+
+        if (greenSquare.y + greenSquare.height >= canvas.height) {
+            greenSquare.dy = -1
+        }
+
+        if (greenSquare.y - greenSquare.height <= 0) {
+            greenSquare.dy = 1
+        }
+
+        greenSquare.x += greenSquare.dx
+        greenSquare.y += greenSquare.dy
+
         greenSquare.render()
+
+    }
+
+    function moveYellowSquare() {
+
+        if (yellowSquare.dx === 1) {
+            if (yellowSquare.x + yellowSquare.width >= canvas.width) {
+                yellowSquare.dx += -1
+            }
+        } else if (yellowSquare.dx === -1) {
+            if (yellowSquare.x - yellowSquare.width <= 0) {
+                yellowSquare.dx += 1
+            }
+        }
+        if (yellowSquare.x + yellowSquare.width >= canvas.width) {
+            yellowSquare.dx = -1
+        }
+
+        if (yellowSquare.x - yellowSquare.width <= 0) {
+            yellowSquare.dx = 1
+        }
+
+        if (yellowSquare.dy === 1) {
+            if (yellowSquare.y + yellowSquare.height >= canvas.height) {
+                yellowSquare.dy += -1
+            }
+        } else if (yellowSquare.dy === -1) {
+            if (yellowSquare.y - yellowSquare.height <= 0) {
+                yellowSquare.dy += 1
+            }
+        }
+
+        if (yellowSquare.y + yellowSquare.height >= canvas.height) {
+            yellowSquare.dy = -1
+        }
+
+        if (yellowSquare.y - yellowSquare.height <= 0) {
+            yellowSquare.dy = 1
+        }
+
+        yellowSquare.x += yellowSquare.dx
+        yellowSquare.y += yellowSquare.dy
+
         yellowSquare.render()
 
     }
 
-    let gameLoop = setInterval(moveObject, 10)
+    function moveRedCircle() {
+
+        if (redCircle.dx === 1) {
+            if (redCircle.x + 12.5 >= canvas.width) {
+                redCircle.dx += -1
+            }
+        } else if (redCircle.dx === -1) {
+            if (redCircle.x - 12.5 <= 0) {
+                redCircle.dx += 1
+            }
+        }
+        if (redCircle.x + 12.5 >= canvas.width) {
+            redCircle.dx = -1
+        }
+
+        if (redCircle.x - 12.5 <= 0) {
+            redCircle.dx = 1
+        }
+
+        if (redCircle.dy === 1) {
+            if (redCircle.y + 12.5 >= canvas.height) {
+                redCircle.dy += -1
+            }
+        } else if (redCircle.dy === -1) {
+            if (redCircle.y - 12.5 <= 0) {
+                redCircle.dy += 1
+            }
+        }
+
+        if (redCircle.y + 12.5 >= canvas.height) {
+            redCircle.dy = -1
+        }
+
+        if (redCircle.y - 12.5 <= 0) {
+            redCircle.dy = 1
+        }
+
+        redCircle.x += redCircle.dx
+        redCircle.y += redCircle.dy
+
+        redCircle.render()
+
+    }
+
+    function moveBlueCircle() {
+
+        if (blueCircle.dx === 1) {
+            if (blueCircle.x + 12.5 >= canvas.width) {
+                blueCircle.dx += -1
+            }
+        } else if (blueCircle.dx === -1) {
+            if (blueCircle.x - 12.5 <= 0) {
+                blueCircle.dx += 1
+            }
+        }
+        if (blueCircle.x + 12.5 >= canvas.width) {
+            blueCircle.dx = -1
+        }
+
+        if (blueCircle.x - 12.5 <= 0) {
+            blueCircle.dx = 1
+        }
+
+        if (blueCircle.dy === 1) {
+            if (blueCircle.y + 12.5 >= canvas.height) {
+                blueCircle.dy += -1
+            }
+        } else if (blueCircle.dy === -1) {
+            if (blueCircle.y - 12.5 <= 0) {
+                blueCircle.dy += 1
+            }
+        }
+
+        if (blueCircle.y + 12.5 >= canvas.height) {
+            blueCircle.dy = -1
+        }
+
+        if (blueCircle.y - 12.5 <= 0) {
+            blueCircle.dy = 1
+        }
+
+        blueCircle.x += blueCircle.dx
+        blueCircle.y += blueCircle.dy
+
+        blueCircle.render()
+
+    }
+
+    function moveGreenCircle() {
+
+        if (greenCircle.dx === 1) {
+            if (greenCircle.x + 12.5 >= canvas.width) {
+                greenCircle.dx += -1
+            }
+        } else if (greenCircle.dx === -1) {
+            if (greenCircle.x - 12.5 <= 0) {
+                greenCircle.dx += 1
+            }
+        }
+        if (greenCircle.x + 12.5 >= canvas.width) {
+            greenCircle.dx = -1
+        }
+
+        if (greenCircle.x - 12.5 <= 0) {
+            greenCircle.dx = 1
+        }
+
+        if (greenCircle.dy === 1) {
+            if (greenCircle.y + 12.5 >= canvas.height) {
+                greenCircle.dy += -1
+            }
+        } else if (greenCircle.dy === -1) {
+            if (greenCircle.y - 12.5 <= 0) {
+                greenCircle.dy += 1
+            }
+        }
+
+        if (greenCircle.y + 12.5 >= canvas.height) {
+            greenCircle.dy = -1
+        }
+
+        if (greenCircle.y - 12.5 <= 0) {
+            greenCircle.dy = 1
+        }
+
+        greenCircle.x += greenCircle.dx
+        greenCircle.y += greenCircle.dy
+
+        greenCircle.render()
+
+    }
+
+    function moveYellowCircle() {
+
+        if (yellowCircle.dx === 1) {
+            if (yellowCircle.x + 12.5 >= canvas.width) {
+                yellowCircle.dx += -1
+            }
+        } else if (yellowCircle.dx === -1) {
+            if (yellowCircle.x - 12.5 <= 0) {
+                yellowCircle.dx += 1
+            }
+        }
+        if (yellowCircle.x + 12.5 >= canvas.width) {
+            yellowCircle.dx = -1
+        }
+
+        if (yellowCircle.x - 12.5 <= 0) {
+            yellowCircle.dx = 1
+        }
+
+        if (yellowCircle.dy === 1) {
+            if (yellowCircle.y + 12.5 >= canvas.height) {
+                yellowCircle.dy += -1
+            }
+        } else if (yellowCircle.dy === -1) {
+            if (yellowCircle.y - 12.5 <= 0) {
+                yellowCircle.dy += 1
+            }
+        }
+
+        if (yellowCircle.y + 12.5 >= canvas.height) {
+            yellowCircle.dy = -1
+        }
+
+        if (yellowCircle.y - 12.5 <= 0) {
+            yellowCircle.dy = 1
+        }
+
+        yellowCircle.x += yellowCircle.dx
+        yellowCircle.y += yellowCircle.dy
+
+        yellowCircle.render()
+
+    }
+
+    let gameLoopRedSquare = setInterval(moveRedSquare, 10)
+    let gameLoopBlueSquare = setInterval(moveBlueSquare, 10)
+    let gameLoopGreenSquare = setInterval(moveGreenSquare, 10)
+    let gameLoopYellowSquare = setInterval(moveYellowSquare, 10)
+
+    let gameLoopRedCircle = setInterval(moveRedCircle, 10)
+    let gameLoopBlueCircle = setInterval(moveBlueCircle, 10)
+    let gameLoopGreenCircle = setInterval(moveGreenCircle, 10)
+    let gameLoopYellowCircle = setInterval(moveYellowCircle, 10)
 
 })
 
